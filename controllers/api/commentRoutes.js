6a16056add114b095097a7a92c9f4b572f52ce58
frenzie24 <kevincharlesgross@gmail.com>
2024-05-26 03:59:45 -0400

    added basic terminal output to commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,38 +1,41 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { log, info, warn, error } = require('@frenzie24/logger')
 
 router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+    info('attempting new comment')
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
-    res.status(200).json(newComment);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+    info(`attempting to delete Comment with id: ${req.params.id}`)
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
 
-    if (!commentData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
+        if (!commentData) {
+            res.status(404).json({ message: 'No project found with this id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
     }
-
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
