c058452b7cf6ee214350a07e519a8196c1c5468b
frenzie24 <kevincharlesgross@gmail.com>
2024-05-26 04:37:15 -0400

    added output to postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,34 +3,41 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
+    info('attempting new Post')
   try {
     const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
+    log('Post created.', 'green')
     res.status(200).json(newPost);
   } catch (err) {
+    error(err)
     res.status(400).json(err);
   }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
+    const _id = req.params.id;
+    info(`attempting to delete Post with id: ${_id}`)
   try {
     const postData = await Post.destroy({
       where: {
-        id: req.params.id,
+        id: _id,
         user_id: req.session.user_id,
       },
     });
 
     if (!postData) {
+        
+        warn(`No Post found with id: ${_id}`);
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
-
+    log('Post deleted', 'green');
     res.status(200).json(postData);
   } catch (err) {
+    error(err)
     res.status(500).json(err);
   }
 });
